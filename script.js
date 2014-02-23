8f63ba5ff580e2b250413b22fd47e095099134aa
Axel Lanuza <axlwolf@gmail.com>
2014-02-23 16:15:30 -0600

    slider implementado

--- script.js
+++ script.js
@@ -1,80 +1,70 @@
-	   // creación del modulo angular llamdo scotchApp
-	  var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	    // creación del modulo angular llamdo scotchApp
+	   var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-	   // configure our routes
-	   //	scotchApp.config(function ($routeProvider, $locationProvider)
-	  scotchApp.config(function ($routeProvider, $locationProvider) {
-	      $routeProvider
+	    // configure our routes
+	    //	scotchApp.config(function ($routeProvider, $locationProvider)
+	   scotchApp.config(function ($routeProvider, $locationProvider) {
+	       $routeProvider
 
-	      // ruta para inicio
-	      .when('/', {
-	          templateUrl: 'paginas/home.html',
-	          controller: 'mainController'
-	      })
+	       // ruta para inicio
+	       .when('/', {
+	           templateUrl: 'paginas/home.html',
+	           controller: 'mainController'
+	       })
 
-	      // ruta para la página nosotros
-	      .when('/nosotros', {
-	          templateUrl: 'paginas/nosotros.html',
-	          controller: 'nosotrosController'
-	          //	        controller: 'mainController'
+	       // ruta para la página nosotros
+	       .when('/nosotros', {
+	           templateUrl: 'paginas/nosotros.html',
+	           controller: 'nosotrosController'
+	           //	        controller: 'mainController'
 
-	      })
+	       })
 
-	      // ruta para la página de galeria
-	      .when('/galeria', {
-	          templateUrl: 'paginas/galeria.html',
-	          controller: 'galeriaController'
-	          //	        controller: 'mainController'
+	       // ruta para la página de galeria
+	       .when('/galeria', {
+	           templateUrl: 'paginas/galeria.html',
+	           controller: 'galeriaController'
+	           //	        controller: 'mainController'
 
-	      })
-	          .when('/contacto', {
-	              templateUrl: 'paginas/contacto.html',
-	              controller: 'contactoController'
-	              //	            controller: 'mainController'
+	       })
+	           .when('/contacto', {
+	               templateUrl: 'paginas/contacto.html',
+	               controller: 'contactoController'
+	               //	            controller: 'mainController'
 
-	          });
-	      //	    $locationProvider.html5Mode(true);
+	           });
+	       //	    $locationProvider.html5Mode(true);
 
-	  });
+	   });
 
-	   //	// create the controller and inject Angular's $scope
-	   //	scotchApp.controller('mainController', function($scope) {
-	   //		// create a message to display in our view
-	   //		$scope.message = 'Everyone come and see how good I look!';
-	   //	});
-	   //
-	   //	scotchApp.controller('aboutController', function($scope) {
-	   //		$scope.message = 'Look! I am an about page.';
-	   //	});
-	   //
-	   //	scotchApp.controller('contactController', function($scope) {
-	   //		$scope.message = 'Contact us! JK. This is just a demo.';
-	   //	});
+	    //	// create the controller and inject Angular's $scope
+	    //	scotchApp.controller('mainController', function($scope) {
+	    //		// create a message to display in our view
+	    //		$scope.message = 'Everyone come and see how good I look!';
+	    //	});
+	    //
+	    //	scotchApp.controller('aboutController', function($scope) {
+	    //		$scope.message = 'Look! I am an about page.';
+	    //	});
+	    //
+	    //	scotchApp.controller('contactController', function($scope) {
+	    //		$scope.message = 'Contact us! JK. This is just a demo.';
+	    //	});
 
-	   // create the controller and inject Angular's $scope
-	  scotchApp.controller('mainController', function ($scope) {
-	      // create a message to display in our view
-	      $scope.message = 'Everyone come and see how good I look!';
-	  });
+	    // create the controller and inject Angular's $scope
+	   scotchApp.controller('mainController', function ($scope) {
+	       // create a message to display in our view
+	       $scope.message = 'Everyone come and see how good I look!';
+	   });
 
-	  scotchApp.controller('nosotrosController', function ($scope) {
-	      $scope.message = 'Look! I am an about page.';
-	  });
-	  scotchApp.controller('galeriaController', function ($scope) {
-	      $scope.message = 'Look! I am an about page.';
-	  });
-	  scotchApp.controller('contactoController', function ($scope) {
-	      $scope.message = 'Look! I am an about page.';
-	  });
+	   scotchApp.controller('nosotrosController', function ($scope) {
+	       $scope.message = 'Look! I am an about page.';
+	   });
+	   scotchApp.controller('galeriaController', function ($scope) {
+	       $scope.message = 'Look! I am an about page.';
+	   });
+	   scotchApp.controller('contactoController', function ($scope) {
+	       $scope.message = 'Look! I am an about page.';
+	   });
 
-	   //llamar el slider
-
-
-	  $(function () {
-	      $('#ei-slider').eislideshow({
-	          animation: 'center',
-	          autoplay: true,
-	          slideshow_interval: 3000,
-	          titlesFactor: 0
-	      });
-	  });
+	 
