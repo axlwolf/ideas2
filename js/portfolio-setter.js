561e1b984fa8b1ffb46e4edad468588a95a3b432
Axel Lanuza <axlwolf@gmail.com>
2014-02-23 19:33:52 -0600

    'galeria'

---
+++ js/portfolio-setter.js
@@ -0,0 +1,24 @@
+﻿eval( function(p, a, c, k, e, d) {
+	e = function(c) {
+		return (c < a ? '' : e(parseInt(c / a))) + (( c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+	};
+	if (!''.replace(/^/, String)) {
+		while (c--) {
+			d[e(c)] = k[c] || e(c)
+		}
+		k = [
+		function(e) {
+			return d[e]
+		}];
+		e = function() {
+			return '\\w+'
+		};
+		c = 1
+	};
+	while (c--) {
+		if (k[c]) {
+			p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+		}
+	}
+	return p
+}('(5($){$.2p.2q=5(4){3 1u={L:12,v:2o,11:2n,K:2k,J:N,1F:\'2l\',1X:\'2m\',24:2r,1t:21,1W:E,1I:\'2s.A\',22:\'2x\',1i:\'2y\',1E:\'2w\',1b:\'1o\'};3 4=$.2v(1u,4);4.2u=$(9);3 w=[],b=[],o=[],W=0,t=0,14=E,D;3 e=$(\'<6 />\').l({z:(4.v*2),19:4.11,25:\'23\'});$(9).l({z:4.v,19:\'2j\',2h:\'28\'}).k(e);3 M=$(9).X(\'n\');1x();5 1x(){e.27().k(\'<6 j="1z"></6>\');1H()}5 1H(){$.26({29:\'2i\',2g:4.1I,2f:\'A\',2e:5(A){c($(A).d(\'2c:x\').B()===\'2d\'){4.J=E}c(4.J){$(A).d(\'w\').2z(0).d(\'m\').1K(5(i){3 q=$(9);3 m={n:q.X(\'n\'),1p:q.B()};w.13(m)})}$(A).d(\'17\').1K(5(i){3 q=$(9);3 10=q.d(\'O:x\').B();3 Z=q.d(\'15:x\').B();3 1B=q.d(\'m:x\').B().2R(\',\');3 Y=q.d(\'16:x\').B();3 17={O:10,15:Z,m:1B,16:Y,p:$(\'<6 j="\'+4.1b+\'"><a 1k="1l[\'+M+\']" j="1Q" 1P="\'+Z+\'" 1O="\'+Y+\'"><I 18="\'+10+\'" /></a></6>\')};b.13(17)});1y()}})}5 1y(){c($.1e.1M){14=N}e.1N(\'.1z\').2W();e.2U(\'<6 n="\'+4.1i+\'"><u></u></6>\');c(4.J){1D()}1w()}5 1D(){D=$(\'<6 n="\'+4.1E+\'"></6>\');D.k(\'<6 n="2F"></6><u></u><6 n="2O"></6>\');e.2D(D);3 V=D.d(\'u\');3 P=$(\'<f>\'+4.1F+\'</f>\');V.k(P);U(P);P.1j({\'1c\':5(){Q();U($(9))},\'1f\':5(){$(9).l({1h:\'1d\'})}});3 1v=w.g;F(3 i=0;i<1v;i++)(5(i){3 m=$(\'<f>\'+w[i].1p+\'</f>\');V.k(m);m.1j({\'1c\':5(){Q(w[i].n);U($(9))},\'1f\':5(){$(9).l({1h:\'1d\'})}})})(i)}5 U(h){3 H=D.d(\'u f.h\');c(H[0]){3 1m=H.d(\'6.1q\').s();H.s(1m);H.1Z(\'h\')}3 s=h.s();h.s(\'<6 j="2I"><6 j="2J"></6><6 j="1q">\'+s+\'</6><6 j="2Q"></6> </6>\');h.1g(\'h\')}5 1w(){3 1r=b.g;F(3 i 2H b){c(b.2G(i)){3 I=2C 2B();$(I).2N(5(){W++;c(W===1r){Q()}}).X(\'18\',b[i].O)}}}5 Q(){3 G=1s.g===0?E:N;e.s(\'\');e.z(2a);o=[];e.y({r:0});3 g=b.g;t=0;3 S=1s[0];F(3 i=0;i<g;i++)(5(i,G,S){c(!G||(G&&b[i].m.1A(S))){c(t%4.L===0){e.z(e.z()+4.v+20);3 1a=$(\'<6 j="\'+4.22+\'"></6>\').l({25:\'23\',z:4.v+4.K,19:4.11});o.13(1a);e.k(1a)}c(14){3 p=$(\'<6 j="\'+4.1b+\'" ><a 1k="1l[\'+M+\']" j="1Q" 1P="\'+b[i].15+\'" 1O="\'+b[i].16+\'"><I 18="\'+b[i].O+\'" /></a></6>\');o[o.g-1].k(p.l({1R:\'1S\'}));b[i].p=p}1V{o[o.g-1].k(b[i].p.l({1R:\'1S\'}))}3 2S=t>=4.L?0:1T;c(t>=4.L||!4.1W){b[i].p.1U().y({R:1},0)}1V{2A(5(){b[i].p.1U().y({R:1},0)},t*1T)}t++}})(i,G,S);$(\'a[1k="1l[\'+M+\']"]\').2t({\'2b\':\'2E\'});1Y();1n()}5 1Y(){3 C=e.1N(\'#\'+4.1i).d(\'u\');C.s(\'\');3 T=o.g;c(T>1){F(3 i=0;i<T;i++)(5(i){3 f=$(\'<f></f>\');C.k(f);f.1j({\'1c\':5(){3 r=i*4.v+i*4.K-2;c($.1e.1M&&$.1e.2L.2K(0,1)<7){r+=i*4.K}e.y({r:[-r,4.1X]},4.24);C.d(\'f.h\').1Z(\'h\');$(9).1g(\'h\')},\'1f\':5(){$(9).l({1h:\'1d\'})}})})(i);C.d(\'f:x\').1g(\'h\');3 r=(4.v)/2-T*4.1t/2;C.l({r:r})}}5 1n(){$(\'.1o\').2M(5(){$(9).1L().y({R:0.8},1G)},5(){$(9).1L().y({R:1},1G)})}}}(2V));2T.2P.1A=5(1C){3 g=9.g;F(3 i=0;i<g;i++){c(9[i]===1C){1J N}}1J E};', 62, 183, '|||var|options|function|div|||this||items|if|find|root|li|length|selected||class|append|css|category|id|pageWrappers|obj|current|marginLeft|html|counter|ul|pageWidth|categories|first|animate|width|xml|text|navUl|categoryHolder|false|for|filterCat|prevSelected|img|showCategories|itemMargin|itemsPerPage|parentId|true|thumbnail|allLink|displayItems|opacity|catId|pageNumber|showSelectedCat|catUl|imagesLoaded|attr|desc|prev|thum|pageHeight||push|ie|preview|description|item|src|height|wrapper|itemClass|click|pointer|browser|mouseover|addClass|cursor|navigationId|bind|rel|lightbox|prevHtml|setHoverFunctionality|portfolio_item|name|port_cat|imageCount|arguments|navButtonWidth|defaults|catNumber|loadImages|init|setSetter|loading|contains|cat|value|displayCategories|categoriesId|allText|300|loadItems|xmlSource|return|each|stop|msie|siblings|title|href|single_image|display|none|100|fadeIn|else|wavyAnimation|easing|showNavigation|removeClass|||pageWrapperClass|left|animationSpeed|float|ajax|parent|hidden|type|200|theme|show_categories|off|success|dataType|url|overflow|GET|auto|30|Todas|easeOutExpo|430|960|fn|portfolioSetter|800|portfolio|prettyPhoto|pageHolder|extend|portfolio_categories|page_wrapper|portfolio_pagination|eq|setTimeout|Image|new|before|dark_rounded|portfolio_categories_left|hasOwnProperty|in|port_cat_active|port_cat_l|substr|version|hover|load|portfolio_categories_right|prototype|port_cat_r|split|timeout|Array|after|jQuery|remove'.split('|'), 0, {}))
---
+++ upload/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<?php
+	include 'includes/header.php';
+	$pageTitle = "Ideas de Hierro | Inicio";
+ ?>
+<!--=================== slider ==================-->
+
+<div class="main">
+	<div id="br" class="banner-rotator">
+		<ul class="br-slides">
+			<li data-link="javascript:void(0)" data-target="_blank">
+				<a href="upload/comedor_1.jpg"> <img src="upload/thumbnails/comedores/tn_comedor_1.jpg" title="comedor 1" alt="comedor 1" /> </a>
+			</li>
+			<li data-link="javascript:void(0)" data-target="_blank">
+				<a href="upload/sala_1.jpg"> <img src="upload/thumbnails/salas/tn_sala_1.jpg" title="sala 1" alt="sala 1" /> </a>
+			</li>
+			<li data-link="javascript:void(0)" data-target="_blank">
+				<a href="upload/recamara_1.jpg"> <img src="upload/thumbnails/recamaras/tn_recamara_1.jpg" title="recamara 1" alt="recamara 1" /> </a>
+			</li>
+			<!--
+			<li data-link="javascript:void(0)" data-target="_blank">
+			<a href="images/ocean.jpg"><img src="images/thumbs/ocean.jpg" title="ocean" alt=""/></a>
+			<h1 data-transition='zoomIn 500 swing 0' data-effect-out='fade'
+			style='top:40px;right:25px;width:auto;height:auto;background:#0e65d5;box-shadow:0 0 3px rgba(0,0,0,.75);'>
+			Ideas de Hierro
+			</h1>
+			<h4 data-transition='zoomIn 500 swing 400' data-effect-out='fade'
+			style='top:110px;right:45px;width:auto;height:auto;background:#fff;color:#333;box-shadow:0 0 3px rgba(0,0,0,.75);'>
+			Lorem ipsum dolor sit amet.                            </h4>
+			<h4 data-transition='zoomIn 500 swing 800' data-effect-out='fade'
+			style='top:160px;right:65px;width:auto;height:auto;background:#fff;color:#333;box-shadow:0 0 3px rgba(0,0,0,.75);'>
+			Lorem ipsum dolor sit amet.                            </h4>
+			<h4 data-transition='zoomIn 500 swing 1200' data-effect-out='fade'
+			style='top:210px;right:25px;width:auto;height:auto;background:#fff;color:#333;box-shadow:0 0 3px rgba(0,0,0,.75);'>
+			Lorem ipsum dolor sit amet.                            </h4>
+			</li>
+			<li data-link="javascript:void(0)" data-target="_blank">
+			<a href="images/triworks_abstract26.jpg"><img src="images/thumbs/triworks_abstract26.jpg" title="3D abstract art" alt=""/></a>
+			<h1 data-transition="slideDown 500 swing 0" data-effect-out='slideDown'
+			style="top:100px;left:10px;width:auto;height:auto;color:#fff;text-shadow:1px 1px 2px rgba(0,0,0,0.2);">
+			Lorem ipsum dolor sit amet.
+			</h1>
+			<h3 data-transition="flipDown 500 swing 0" data-transition-out="flipDown 500 swing 1000"
+			style="top:170px;left:15px;width:auto;height:auto;background:#00ccff;background-color:rgba(0,204,255,0.95);padding:5px 15px">
+			Lorem ipsum dolor sit amet.                            </h3>
+			<h3 data-transition="flipDown 500 swing 2000" data-transition-out="flipDown 500 swing 1000"
+			style="top:170px;left:15px;width:auto;height:auto;background:#9966ff;background-color:rgba(153,102,255,0.95);padding:5px 15px">
+			Lorem ipsum dolor sit amet.
+			</h3>
+			<h3 data-transition="flipDown 500 swing 4000" data-transition-out="flipDown 500 swing"
+			style="top:170px;left:15px;width:auto;height:auto;background:#99ff00;background-color:rgba(153,255,0,0.95);padding:5px 15px">
+			Lorem ipsum dolor sit amet.                            </h3>
+			</li>
+			<li data-link="javascript:void(0)" data-target="_blank">
+			<a href="images/temple.jpg"><img src="images/thumbs/temple.jpg" title="temple" alt=""/></a>
+			<h3 data-transition="flipUp 500 linear" data-effect-out="flipDown"
+			style="top:30px;left:25px;background-color:#000;width:auto;height:auto;letter-spacing:20px;padding:5px 0 5px 20px;">
+			Ideas
+			</h3>
+			<h3 data-transition="flipDown 500 linear 400" data-effect-out="flipUp"
+			style="top:95px;left:25px;background-color:#000;width:auto;height:auto;letter-spacing:20px;padding:5px 0 5px 20px;">
+			de
+			</h3>
+			<h3 data-transition="flipUp 500 linear 800" data-effect-out="flipDown"
+			style="top:160px;left:25px;background-color:#000;width:auto;height:auto;letter-spacing:20px;padding:5px 0 5px 20px;">
+			Hierro
+			</h3>
+			<h3 data-transition="flipDown 500 linear 1200" data-effect-out="flipUp"
+			style="top:225px;left:25px;background-color:#000;width:auto;height:auto;letter-spacing:20px;padding:5px 0 5px 20px;">
+			2013
+			</h3>
+			</li>
+			<li data-link="javascript:void(0)" data-target="_blank">
+			<a href="images/flowers.jpg"><img src="images/thumbs/flowers.jpg" title="flowers" alt=""/></a>
+			<h2 data-transition='slideUp 500 swing 0' data-transition-out='slideUp'
+			style='top:15px;left:15px;width:auto;height:auto;color:#fff;background:#3095fc;'>
+			Ideas de Hierro
+			</h2>
+			<h4 data-transition='slideLeft 500 swing 500' data-transition-out='slideLeft'
+			style='top:85px;left:15px;width:auto;height:auto;color:#333;background:#fff;background-color:rgba(255,255,255,0.9)'>
+			Lorem ipsum dolor sit amet.
+			</h4>
+			<h4 data-transition='slideRight 500 swing 500' data-transition-out='slideRight'
+			style='top:135px;left:15px;width:auto;height:auto;color:#333;background:#fff;background-color:rgba(255,255,255,0.9)'>
+			Lorem ipsum dolor sit amet.
+			</h4>
+			<h4 data-transition='slideLeft 500 swing 500' data-transition-out='slideLeft'
+			style='top:185px;left:15px;width:auto;height:auto;color:#333;background:#fff;background-color:rgba(255,255,255,0.9)'>
+			Lorem ipsum dolor sit amet.                            </h4>
+			</li>-->
+
+		</ul>
+	</div>
+
+</div>
+</header>
+<!--======================== content ===========================-->
+<section id="content">
+	<div class="container_24">
+		<div class="grid_24">
+			<div class="indent-bottom9-1">
+				<div class="grid_12 alpha">
+					<div class="dgrid_6">
+						<div class="box1">
+							<h3>
+							<a="h3s">
+								Salas</a>
+								<br>
+								<br>
+							</h3>
+							<div class="indents">
+								<figure class="p2-1">
+									<span class="img-box1"> <img src="upload/thumbnails/salas/tn_sala_1.jpg" alt="sala 01"> </span>
+								</figure>
+								<a href="galeria.php" class="button">visita la galería</a>
+							</div>
+						</div>
+					</div>
+					<div class="dgrid_6">
+						<div class="box1">
+							<h3>Recamaras
+							<br>
+							<br>
+							</h3>
+							<div class="indents">
+								<figure class="p2-1">
+									<span class="img-box1"> <img src="upload/thumbnails/recamaras/tn_recamara_1.jpg" alt=""> </span>
+								</figure>
+								<a href="galeria.php" class="button">visita la gallería</a>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="grid_12 omega">
+					<div class="dgrid_6">
+						<div class="box1">
+							<h3>Comedores
+							<br>
+							<br>
+							</h3>
+							<div class="indents">
+								<figure class="p2-1">
+									<span class="img-box1"> <img src="upload/thumbnails/comedores/tn_comedor_1.jpg" alt=""> </span>
+								</figure>
+								<a href="galeria.php" class="button">visita la galería</a>
+							</div>
+						</div>
+					</div>
+					<div class="dgrid_6">
+						<div class="box1">
+							<h3>Muebles
+							<br>
+							Auxiliares </h3>
+							<div class="indents">
+								<figure class="p2-1">
+									<span class="img-box1"> <img src="upload/thumbnails/muebles auxiliares/tn_CAM00389.jpg" alt=""> </span>
+								</figure>
+								<a href="galeria.php" class="button">visita la gallería</a>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="indent-bottom9 border-bottom p8-1">
+				<div class="border-left9">
+					<div class="wrapper border-left19">
+						<div class="grid_12 alpha">
+							<div class="prefix_1 indent-right6 indent-top1-1">
+								<h3 class="p7-1">Proyectos Recientes</h3>
+								<div id="carousel" class="es-carousel-wrapper">
+									<div class="es-carousel">
+										<ul>
+											<li>
+												<figure class="p3">
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+												<figure>
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+											</li>
+											<li>
+												<figure class="p3">
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+												<figure>
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+											</li>
+											<li>
+												<figure class="p3">
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+												<figure>
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img1.JPG" alt=""> </a> </span>
+												</figure>
+											</li>
+											<li>
+												<figure class="p3">
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img3.JPG" alt=""> </a> </span>
+												</figure>
+												<figure>
+													<span class="img-box2"> <img src="images/img-shadow.png" alt="" class="img-shadow"> <a href="#" class="img-border"> <img src="images/page1-img3.JPG" alt=""> </a> </span>
+												</figure>
+											</li>
+										</ul>
+										<div class="clear"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid_10 omega">
+							<div class="prefix_1 indent-top1-1">
+								<h3 class="p7-1">Testimonios</h3>
+								<div class="p5">
+									<blockquote>
+										<div class="quote-content">
+											<div class="quote-light">
+												“ Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus.”
+											</div>
+										</div>
+									</blockquote>
+									<div class="quote-caption">
+										<h6>Linda Priston</h6>
+										Praesent vestibulum molestie
+									</div>
+								</div>
+								<a href="#" class="button">más testimonios</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="border-left18">
+
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</section>
+<?php
+	include 'includes/footer.php';
+ ?>
+
+</html>
